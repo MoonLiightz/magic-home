9fa35575e2229fb3a8ee8c7b3cb097c60fa3710c
Patrick Stebbe <info@moonliightz.de>
2021-06-05 16:58:09 +0200

    feat(internal): add some utils
    - Min
    - Max
    - Clamp

--- internal/util/util.go
+++ internal/util/util.go
@@ -7,3 +7,24 @@ func ParseStringToUint8(v string) uint8 {
 	res, _ := strconv.ParseUint(v, 10, 8)
 	return uint8(res)
 }
+
+// Min compares the two numbers and returns the smaller number
+func Min(x uint8, y uint8) uint8 {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+// Max compares the two numbers and returns the larger number
+func Max(x uint8, y uint8) uint8 {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+// Clamp "clamps" a value between a pair of boundary values
+func Clamp(value uint8, minValue uint8, maxValue uint8) uint8 {
+	return Min(maxValue, Max(minValue, value))
+}
