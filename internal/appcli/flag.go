565020f4f63d45ff1cca36757e9a694d820e15c6
Patrick Stebbe <info@moonliightz.de>
2021-07-20 17:02:54 +0200

    feat(cli): add some flags
    This adds the following new flags:
    - verbose
    - cw-support
    - masks

--- internal/appcli/flag.go
+++ internal/appcli/flag.go
@@ -7,6 +7,9 @@ type flag struct {
 	BroadcastAddr *cli.StringFlag
 	Timeout       *cli.IntFlag
 	JSON          *cli.BoolFlag
+	Verbose       *cli.BoolFlag
+	CWSupport     *cli.BoolFlag
+	Masks         *cli.BoolFlag
 }
 
 // Flag contains the options for the CLI
@@ -14,7 +17,7 @@ var Flag = flag{
 	Port: &cli.IntFlag{
 		Name:        "port",
 		Aliases:     []string{"p"},
-		Usage:       "socket port of the LED Strip",
+		Usage:       "socket port of the device",
 		DefaultText: "5577",
 		Value:       5577,
 		Required:    false,
@@ -39,6 +42,30 @@ var Flag = flag{
 		Name:        "json",
 		Aliases:     []string{},
 		Usage:       "return output as a JSON-String",
+		DefaultText: "false",
+		Value:       false,
+		Required:    false,
+	},
+	Verbose: &cli.BoolFlag{
+		Name:        "verbose",
+		Aliases:     []string{},
+		Usage:       "verbose mode to output things like sent and received bytes",
+		DefaultText: "false",
+		Value:       false,
+		Required:    false,
+	},
+	CWSupport: &cli.BoolFlag{
+		Name:        "cw-support",
+		Aliases:     []string{},
+		Usage:       "enables support for setting cold white values",
+		DefaultText: "false",
+		Value:       false,
+		Required:    false,
+	},
+	Masks: &cli.BoolFlag{
+		Name:        "masks",
+		Aliases:     []string{},
+		Usage:       "enables a special bitmask which is required for some devices of type 0x25, 0x35 or 0x44",
 		DefaultText: "false",
 		Value:       false,
 		Required:    false,
